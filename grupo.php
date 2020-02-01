cac9437070b5b19b8a0157375e678c6f63756fe6
Joaquín <joaquin@muruais.com>
2020-02-01 11:45:37 +0100

    Corregido error

--- grupo.php
+++ grupo.php
@@ -84,7 +84,7 @@ function GuardarVotos(){
     <head>
         <meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Eurovision - <?php echo $name_group; echo $papafrita;?></title>
+        <title>Eurovision - <?php echo $name_group;?></title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/theme.bootstrap_4.css">
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
