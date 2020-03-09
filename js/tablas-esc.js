1d0efeb64c3b923aa32d311483f4e650b64a901d
Quincho <joaquin@muruais.com>
2020-03-09 21:03:45 +0100

    Añadido index y eliminado ordenado por parte del usuario

--- js/tablas-esc.js
+++ js/tablas-esc.js
@@ -1,8 +1,20 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#clasificacion').DataTable( {
-	paging: false,
-	"searching": false,
-	"order": [[2, 'desc']]
-  });
+	var t = $('#clasificacion').DataTable( {
+        "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": "_all"
+        } ],
+        "order": [[ 2, 'desc' ]],
+		paging: false,
+		"searching": false,
+		"sInfo": ""
+    } );
+ 
+    t.on( 'order.dt search.dt', function () {
+        t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+        } );
+    } ).draw();
 });
