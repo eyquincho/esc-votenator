d29cb14df60256fc5b996e34bf5cfe62fc99bec1
Quincho <joaquin@muruais.com>
2020-04-13 19:36:50 +0200

    Añadida la tabla esc_puntuaciones

--- db/crear_tablas.sql
+++ db/crear_tablas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:3306
--- Tiempo de generación: 17-05-2019 a las 19:51:02
--- Versión del servidor: 5.6.41-84.1
--- Versión de PHP: 7.2.7
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-04-2020 a las 18:56:41
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `koiora_eurovision`
+-- Base de datos: `ESC`
 --
 
 -- --------------------------------------------------------
@@ -35,7 +35,6 @@ CREATE TABLE `esc_grupos` (
   `creacion` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `esc_participantes`
@@ -56,7 +55,77 @@ CREATE TABLE `esc_participantes` (
 
 INSERT INTO `esc_participantes` (`id`, `pais`, `iso3`, `artista`, `cancion`, `video`) VALUES
 (1, 'Malta', 'MAL', 'Michela', 'Chameleon', 'https://youtube.com/watch?v=mRwLt9HCjbk'),
-(2, 'Suecia', 'SWE', 'John Lundvik', 'Too Late For Love', 'https://youtube.com/watch?v=YKR2GiorZuQ');
+(2, 'Suecia', 'SWE', 'John Lundvik', 'Too Late For Love', 'https://youtube.com/watch?v=YKR2GiorZuQ'),
+(3, 'Malta', 'MAL', 'Michela', 'Chameleon', 'https://youtube.com/watch?v=mRwLt9HCjbk'),
+(4, 'Suecia', 'SWE', 'John Lundvik', 'Too Late For Love', 'https://youtube.com/watch?v=YKR2GiorZuQ'),
+(5, 'Malta', 'MAL', 'Michela', 'Chameleon', 'https://youtube.com/watch?v=mRwLt9HCjbk'),
+(6, 'Suecia', 'SWE', 'John Lundvik', 'Too Late For Love', 'https://youtube.com/watch?v=YKR2GiorZuQ'),
+(7, 'Malta', 'MAL', 'Michela', 'Chameleon', 'https://youtube.com/watch?v=mRwLt9HCjbk'),
+(8, 'Suecia', 'SWE', 'John Lundvik', 'Too Late For Love', 'https://youtube.com/watch?v=YKR2GiorZuQ'),
+(9, 'Malta', 'MAL', 'Michela', 'Chameleon', 'https://youtube.com/watch?v=mRwLt9HCjbk'),
+(10, 'Suecia', 'SWE', 'John Lundvik', 'Too Late For Love', 'https://youtube.com/watch?v=YKR2GiorZuQ');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `esc_puntuaciones`
+--
+
+CREATE TABLE `esc_puntuaciones` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `juez` varchar(30) NOT NULL,
+  `grupo` varchar(30) NOT NULL,
+  `1` int(2) NOT NULL DEFAULT '0',
+  `2` int(2) NOT NULL DEFAULT '0',
+  `3` int(2) NOT NULL DEFAULT '0',
+  `4` int(2) NOT NULL DEFAULT '0',
+  `5` int(2) NOT NULL DEFAULT '0',
+  `6` int(2) NOT NULL DEFAULT '0',
+  `7` int(2) NOT NULL DEFAULT '0',
+  `8` int(2) NOT NULL DEFAULT '0',
+  `9` int(2) NOT NULL DEFAULT '0',
+  `10` int(2) NOT NULL DEFAULT '0',
+  `11` int(2) NOT NULL DEFAULT '0',
+  `12` int(2) NOT NULL DEFAULT '0',
+  `13` int(2) NOT NULL DEFAULT '0',
+  `14` int(2) NOT NULL DEFAULT '0',
+  `15` int(2) NOT NULL DEFAULT '0',
+  `16` int(2) NOT NULL DEFAULT '0',
+  `17` int(2) NOT NULL DEFAULT '0',
+  `18` int(2) NOT NULL DEFAULT '0',
+  `19` int(2) NOT NULL DEFAULT '0',
+  `20` int(2) NOT NULL DEFAULT '0',
+  `21` int(2) NOT NULL DEFAULT '0',
+  `22` int(2) NOT NULL DEFAULT '0',
+  `23` int(2) NOT NULL DEFAULT '0',
+  `24` int(2) NOT NULL DEFAULT '0',
+  `25` int(2) NOT NULL DEFAULT '0',
+  `26` int(2) NOT NULL DEFAULT '0',
+  `27` int(2) NOT NULL DEFAULT '0',
+  `28` int(2) NOT NULL DEFAULT '0',
+  `29` int(2) NOT NULL DEFAULT '0',
+  `30` int(2) NOT NULL DEFAULT '0',
+  `31` int(2) NOT NULL DEFAULT '0',
+  `32` int(2) NOT NULL DEFAULT '0',
+  `33` int(2) NOT NULL DEFAULT '0',
+  `34` int(2) NOT NULL DEFAULT '0',
+  `35` int(2) NOT NULL DEFAULT '0',
+  `36` int(2) NOT NULL DEFAULT '0',
+  `37` int(2) NOT NULL DEFAULT '0',
+  `38` int(2) NOT NULL DEFAULT '0',
+  `39` int(2) NOT NULL DEFAULT '0',
+  `40` int(2) NOT NULL DEFAULT '0',
+  `41` int(2) NOT NULL DEFAULT '0',
+  `42` int(2) NOT NULL DEFAULT '0',
+  `43` int(2) NOT NULL DEFAULT '0',
+  `44` int(2) NOT NULL DEFAULT '0',
+  `45` int(2) NOT NULL DEFAULT '0',
+  `46` int(2) NOT NULL DEFAULT '0',
+  `47` int(2) NOT NULL DEFAULT '0',
+  `48` int(2) NOT NULL DEFAULT '0',
+  `49` int(2) NOT NULL DEFAULT '0',
+  `50` int(2) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Índices para tablas volcadas
@@ -75,6 +144,12 @@ ALTER TABLE `esc_participantes`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `esc_puntuaciones`
+--
+ALTER TABLE `esc_puntuaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -82,7 +157,13 @@ ALTER TABLE `esc_participantes`
 -- AUTO_INCREMENT de la tabla `esc_grupos`
 --
 ALTER TABLE `esc_grupos`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+
+--
+-- AUTO_INCREMENT de la tabla `esc_puntuaciones`
+--
+ALTER TABLE `esc_puntuaciones`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
